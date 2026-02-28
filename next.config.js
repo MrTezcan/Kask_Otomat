17f85b2467682fce5e3b79f8066579bfc4047937
Kask Otomat <kask@otomat.local>
2026-02-28 17:42:15 +0300

    chore: add cache-control headers to prevent stale layout caching

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/user',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      }
+    ]
+  },
+};
 
 module.exports = nextConfig;
